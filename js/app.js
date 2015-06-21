a88e936e452e0a3b724a03a65165cfdbe6bc9ca3
parag diwan <paragdiwan@gmail.com>
2015-06-21 21:14:51 +0100

    Removed unwanted code & updated angular version

--- js/app.js
+++ js/app.js
@@ -6,37 +6,4 @@ angular.module('demoApp', ['pdTypeAhead'])
             console.log('name retrieval failed.');
         });
 	}
-]);	
-
-
-/*modServiceDef.directive('typeAhead',[function() {
-	return {
-		scope : {
-			cinfo:'=datalist',
-		},
-		link:function(scope,element,attr) {
-			scope.visible = true;
-			scope.updateSearchTerm = function(selName) {
-				scope.sterm = selName;
-			}
-		},
-		templateUrl:'templates/typeahead.tmpl'
-	}
-}]);*/
-
-/*modServiceDef.factory('myFactory', function($http,$q) {
-	var factory = {};
-	var def = $q.defer();
-	factory.getData = function() {
-		$http.get('https://raw.githubusercontent.com/dominictarr/random-name/master/first-names.json')
-			.success(function(res){ 
-				factory.data = res;
-				def.resolve(res);
-			})
-			.error(function(err){
-				def.reject(err);
-			})
-			return def.promise;
-		}
-	return factory;
-});*/
+]);
